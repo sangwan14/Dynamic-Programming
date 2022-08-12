0108e1cb4ff09dccc2d1e221da0d4cfc4f34a5f7
sangwan14 <68826136+sangwan14@users.noreply.github.com>
2022-08-12 18:01:25 +0530

    Update CountSubsetsWithSumK.cpp

--- CountSubsetsWithSumK.cpp
+++ CountSubsetsWithSumK.cpp
@@ -1,3 +1,62 @@
+//Memoization
+class Solution{
+  int findWaysUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+      if(ind==0){
+          return (arr[ind]==target) + (target==0);
+      }
+
+      if(dp[ind][target]!=-1)
+          return dp[ind][target];
+
+      int notTaken = findWaysUtil(ind-1,target,arr,dp);
+
+      int taken = 0;
+      if(arr[ind]<=target)
+          taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
+
+      return dp[ind][target] = notTaken + taken;
+  }
+
+  int findWays(vector<int> &num, int k){
+      int n = num.size();
+      vector<vector<int>> dp(n,vector<int>(k+1,-1));
+      return findWaysUtil(n-1,k,num,dp);
+  }
+};
+//TC : O(N*K)
+//SC : O(N*K) [DP array] + O(N) [Stack Space]
+
+//Tabulation
+class Solution2{
+  int findWays(vector<int> &num, int k){
+       int n = num.size();
+
+      vector<vector<int>> dp(n,vector<int>(k+1,0));
+      
+      dp[0][0] = 1;
+    
+      if(num[0]<=k)
+          dp[0][num[0]] += 1;
+    
+      for(int ind = 1; ind<n; ind++){
+          for(int target= 0; target<=k; target++){
+
+              int notTaken = dp[ind-1][target];
+
+              int taken = 0;
+                  if(num[ind]<=target)
+                      taken = dp[ind-1][target-num[ind]];
+
+              dp[ind][target]= notTaken + taken;
+          }
+      }
+
+      return dp[n-1][k];
+  }
+};
+//TC : O(N*K)
+//SC : O(N*K) [DP array]
+
 //Space Optimized
 class Solution{
   int findWays(vector<int> &num, int k)
