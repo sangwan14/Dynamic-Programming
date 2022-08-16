ea913935c4a43e1013b7ef2250f576c43064f4f7
sangwan14 <68826136+sangwan14@users.noreply.github.com>
2022-08-16 13:08:43 +0530

    Update LongestCommonSubsequence.cpp

--- LongestCommonSubsequence.cpp
+++ LongestCommonSubsequence.cpp
@@ -1,12 +1,14 @@
 //Space Optimized
 class Solution {
 public:
-    int longestCommonSubsequence(string &a, string &b) {
-        short m[2][1000] = {};
-        for (int i = 0; i < a.size(); ++i)
-            for (int j = 0; j < b.size(); ++j)
-                m[!(i % 2)][j + 1] = a[i] == b[j] ? m[i % 2][j] + 1 : max(m[i % 2][j + 1], m[!(i % 2)][j]);
-        return m[a.size() % 2][b.size()];
+    int lcs(string &a, string &b) {
+        int n = a.size();
+        int m = b.size();
+        vector<vector<int>> dp(2, vector<int> (m+1,0));
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < m; ++j)
+                dp[!(i % 2)][j + 1] = a[i] == b[j] ? dp[i % 2][j] + 1 : max(dp[i % 2][j + 1], dp[!(i % 2)][j]);
+        return dp[n % 2][m];
     }
 };
 //TC : O(N*M)
