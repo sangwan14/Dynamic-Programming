80e41ad637f3df3c5a81a46c26c50ba23e5ca791
sangwan14 <68826136+sangwan14@users.noreply.github.com>
2022-08-16 13:10:57 +0530

    Create MinimumNumberOfInsertionsAndDeletions.cpp

---
+++ MinimumNumberOfInsertionsAndDeletions.cpp
@@ -0,0 +1,19 @@
+//Space Optimized
+class Solution{
+  int lcs(string &a, string &b) {
+      int n = a.size();
+      int m = b.size();
+      vector<vector<int>> dp(2, vector<int> (m+1,0));
+      for (int i = 0; i < n; ++i)
+          for (int j = 0; j < m; ++j)
+              dp[!(i % 2)][j + 1] = a[i] == b[j] ? dp[i % 2][j] + 1 : max(dp[i % 2][j + 1], dp[!(i % 2)][j]);
+      return dp[n % 2][m];
+  }
+
+  int canYouMake(string &str, string &ptr)
+  {
+      int n = str.size() + ptr.size() - 2*lcs(str,ptr);
+  }
+};
+//TC : O(N*M)
+//SC : O(min(N,M))
