db9861bb83218bbb2100ff258041a6523f2224cc
sangwan14 <68826136+sangwan14@users.noreply.github.com>
2022-08-19 13:04:46 +0530

    Update BestTimeToBuySellStock2.cpp

--- BestTimeToBuySellStock2.cpp
+++ BestTimeToBuySellStock2.cpp
@@ -1,3 +1,41 @@
+//Memoization
+class Solution{
+  long getAns(long *Arr, int ind, int buy, int n, vector<vector<long>> &dp ){
+
+      if(ind==n) return 0; //base case
+
+      if(dp[ind][buy]!=-1)
+          return dp[ind][buy];
+
+      long profit;
+
+      if(buy==0){// We can buy the stock
+          profit = max(0+getAns(Arr,ind+1,0,n,dp), -Arr[ind] + getAns(Arr,ind+1,1,n,dp));
+      }
+
+      if(buy==1){// We can sell the stock
+          profit = max(0+getAns(Arr,ind+1,1,n,dp), Arr[ind] + getAns(Arr,ind+1,0,n,dp));
+      }
+
+      return dp[ind][buy] = profit;
+  }
+};
+//TC : O(N*2)
+//SC : O(N*2) + O(N)
+
+
+long getMaximumProfit(long *Arr, int n)
+{
+    //Write your code here
+    
+    vector<vector<long>> dp(n,vector<long>(2,-1));
+    
+    if(n==0) return 0;
+    long ans = getAns(Arr,0,0,n,dp);
+    return ans;
+}
+          
+};
 //Tabulation
 class Solution2{
   long getMaximumProfit(long *Arr, int n)
