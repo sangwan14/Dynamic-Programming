4bd64585f254655fadb35582fc9484efd38b7868
sangwan14 <68826136+sangwan14@users.noreply.github.com>
2022-08-24 13:04:33 +0530

    Update LongestBitonicSubsequence.cpp

--- LongestBitonicSubsequence.cpp
+++ LongestBitonicSubsequence.cpp
@@ -12,16 +12,14 @@ class Solution{
 	                dp1[i] = 1+dp1[prev];
 	        }
 	    }
+		
+	    int maxi = 1;
 	    for(int i=n-1; i>=0; i--){
 	        for(int next=n-1; next>i; next--){
 	            if(nums[i]>nums[next] and 1+dp2[next]>dp2[i])
 	                dp2[i] = 1+dp2[next];
 	        }
-	    }
-	    
-	    int maxi = 1;
-	    for(int i=0; i<n; i++){
-	        maxi = max(dp1[i]+dp2[i]-1,maxi);
+	    	maxi = max(dp1[i]+dp2[i]-1,maxi);
 	    }
 	    
 	    return maxi;
